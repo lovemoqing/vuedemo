9556f8442120e475b9e3dcd72a52efc86e56ffb4
默卿的Github <tccwpl@163.com>
2018-02-11 10:23:25 +0800

    vuedemo
    [本地文件上传] 一个使用vue构建的demo，包含一些vue的基本语法，以及路由的设置和异步数据的读取。

---
+++ VueTest/webForm/Handler.ashx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using webForm.Models;
+namespace webForm
+{
+    /// <summary>
+    /// Handler 的摘要说明
+    /// </summary>
+    public class Handler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string type = context.Request["type"] ?? "";
+            int typeid = 0;
+            int.TryParse(context.Request["typeid"], out typeid);
+            int orderid = 0;
+            int.TryParse(context.Request["orderid"], out orderid);
+            switch (type)
+            {
+                case "type":
+                    context.Response.Write(JsonConvert.SerializeObject(GetTypeList()));
+                    break;
+                case "prodlist":
+                    context.Response.Write(JsonConvert.SerializeObject(GetProductList(typeid, orderid)));
+                    break;
+                default:
+                    break;
+            }
+        }
+        public List<CateGoryType> GetTypeList()
+        {
+            List<CateGoryType> list = new List<CateGoryType>() { 
+                new CateGoryType { ID = 1, Name = "Finished Fabric", Href = "http://localhost:8107/Home.html" }, 
+                new CateGoryType { ID = 2, Name = "Elastic Woven Satin", Href = "http://localhost:8107/Home.html" }, 
+                new CateGoryType { ID = 3, Name = "Ice Tulle", Href = "http://localhost:8107/Home.html" },
+                new CateGoryType { ID = 4, Name = "Matte Satin", Href = "http://localhost:8107/Home.html" },
+                new CateGoryType { ID = 5, Name = "Organza", Href = "http://localhost:8107/Home.html" }
+            };
+            return list;
+        }
+
+        public List<Product> GetProductList(int typeid, int orderid)
+        {
+            List<Product> list = new List<Product>() { 
+                new Product { ID = 1, Title = typeid+"Casual Plain Soft PU Men's Bag"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/1.png" }, 
+                new Product { ID = 2, Title = typeid+"Versatile Canvas Zipper Men's Bag"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/2.png" }, 
+                new Product { ID = 3, Title = typeid+"Casual Plain Canvas Men's Bag"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/3.png" }, 
+                new Product { ID = 4, Title = typeid+"Concise Sewing Thread Plain Men's Bag"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/1.png" }, 
+                new Product { ID = 5, Title = typeid+"Casual Plain Nylon Men's Bag"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/2.png" }, 
+                new Product { ID = 6, Title = typeid+"Huge Space Plain Zipper Men's Bag"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/3.png" }, 
+                new Product { ID = 7, Title = typeid+"Casual Tassel Thread Women Satchel"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/1.png" }, 
+                new Product { ID = 8, Title = typeid+"Concise Short Type Women Purse"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/2.png" }, 
+                new Product { ID = 9, Title = typeid+"Fashion Plain Women PU Tote Bag"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/3.png" }, 
+                new Product { ID = 10, Title = typeid+"Casual Rivet Thread Women Tote Bag"+orderid, Href = "http://localhost:8107/Home.html",ImageUrl="/Images/1.png" }, 
+            };
+            return list;
+        }
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ VueTest/webForm/Models/Type.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webForm.Models
+{
+    public class CateGoryType
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+        public string Href { set; get; }
+    }
+
+
+    public class Product
+    {
+        public int ID { set; get; }
+        public string Title { set; get; }
+        public string Href { set; get; }
+        public string Image { set; get; }
+        public string ImageUrl { set; get; }
+        
+    }
+}
